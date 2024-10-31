bc3439579d92d607606ac4fbc418d862637c5ebc
juanma <juanaco1.p@gmail.com>
2024-10-31 16:33:18 +0100

    landing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,8 +4,13 @@ import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  @Get()
+  async getIndex(@Res() res: Response) {
+    return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  }
+
   @Get(':id')
   async getMap(@Param('id') id: string, @Res() res: Response) {
-    return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    return res.sendFile(path.join(__dirname, '..', 'public', 'map.html'));
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
 
@@ -16,17 +14,9 @@ import { AppGateway } from './app.gateway';
         uri: process.env.MONGO_URI,
       }),
     }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-      exclude: ['/whatsapp/*'],
-      serveRoot: '/',
-    }),
   ],
   controllers: [AppController],
   providers: [
-    AppGateway,
-  ],
-  exports: [
     AppGateway,
   ],
 })
