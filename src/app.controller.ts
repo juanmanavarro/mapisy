f608f5b9ba994879c462f8962a827b2ad9817a82
juanma <juanaco1.p@gmail.com>
2024-10-31 16:29:53 +0100

    serve map

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import axios from 'axios';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import * as path from 'path';
+import { Response } from 'express';
 
-@Controller('map')
+@Controller()
 export class AppController {
-  @Get()
-  async getMap() {
-    try {
-      const response = await axios.get(`https://nocodb.juanma.app/api/v2/tables/${process.env.NOCODB_TABLE_ID}/records`, {
-        params: {
-          limit: 1000,
-          shuffle: 0,
-          offset: 0
-        },
-        headers: {
-          'accept': 'application/json',
-          'xc-token': process.env.NOCODB_API_KEY
-        }
-      });
-      return response.data;
-    } catch (error) {
-      console.error(error);
-      return 'error';
-    }
+  @Get(':id')
+  async getMap(@Param('id') id: string, @Res() res: Response) {
+    return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
   }
 }
